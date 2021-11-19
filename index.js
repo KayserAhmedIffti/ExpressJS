5c56a5a7d0deb85fd8778024f718383249a91281
Kayser Ahmed <kayser.ahmed@northsouth.edu>
2021-11-19 20:57:09 +0600

    Add files via upload
    Calculator

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+
+
+app.get("/",(req,res) =>{
+    res.sendFile(__dirname+"/index.html");
+});
+
+
+
+
+app.get("/circle",(req,res) =>{
+    res.sendFile(__dirname+"/circle.html");
+});
+
+
+app.get("/triangle",(req,res) =>{
+    res.sendFile(__dirname+"/triangle.html");
+});
+
+app.post("/triangle",(req,res) =>{
+    const height = req.body.height;
+    const base = req.body.base;
+    const area = (0.5*base*height);
+    res.send('<h2>Area of triangle is </h2>',area);
+
+});
+
+app.post("/circle",(req,res) =>{
+    const radius = req.body.radius;
+    
+    const area = (Math.PI*radius*radius);
+    res.send('Area of Circle is $area',area);
+
+});
+
+
+
+app.listen(PORT,()=>{
+
+    console.log('Serveris is running at http://localhost:',PORT);
+
+})
